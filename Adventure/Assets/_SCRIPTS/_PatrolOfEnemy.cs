7b394360a465e06420305d1b4fc7643c7a514634
Mayntu <daniilkhusnullin@gmail.com>
2022-05-19 23:46:23 +0600

    init project commit
    dada

--- Adventure/Assets/_SCRIPTS/_PatrolOfEnemy.cs
+++ Adventure/Assets/_SCRIPTS/_PatrolOfEnemy.cs
@@ -8,29 +8,66 @@ public class _PatrolOfEnemy : MonoBehaviour
     [SerializeField] private int positionOfPatrol;
     public Transform point;
     private bool movingRight;
+    //private bool isRight;
+    Transform player;
+    [SerializeField] private float stoppingDistance;
+    private bool chill = false;
+    private bool angry = false;
+    private bool goBack = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(transform.position, point.position) < positionOfPatrol)
+        if(Vector2.Distance(transform.position, point.position) < positionOfPatrol && angry == false)
+        {
+            chill = true;
+        }
+        if (Vector2.Distance(transform.position, player.position) < stoppingDistance);
+        {
+            angry = true;
+            chill = false;
+            goBack = false;
+        }
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance);
+        {
+            goBack = true;
+            angry = false;
+        }
+
+
+        if (chill == true)
         {
             Chill();
         }
+        else if(angry == true)
+        {
+            Angry();
+        }
+        else if(goBack == true)
+        {
+            GoBack();
+        }
     }
+    //void FixedUpdate()
+    //{
+    //    IsRight();
+    //}
     void Chill()
     {
         if(transform.position.x > point.position.x + positionOfPatrol)
         {
-            movingRight = true;
+            movingRight = false;
+            //isRight = false;
         }
         else if(transform.position.x < point.position.x - positionOfPatrol)
         {
-            movingRight = false;
+            movingRight = true;
+            //isRight = true;
         }
         if (movingRight)
         {
@@ -43,10 +80,21 @@ public class _PatrolOfEnemy : MonoBehaviour
     }
     void Angry()
     {
-
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     }
     void GoBack()
     {
-
+        transform.position = Vector2.MoveTowards(transform.position, point.position, speed * Time.deltaTime);
     }
+    //public void IsRight()
+    //{
+    //    if (isRight == true)
+    //    {
+    //        GetComponent<SpriteRenderer>().flipX = false;
+    //    }
+    //    else
+    //    {
+    //        GetComponent<SpriteRenderer>().flipX = true;
+    //    }
+    //}
 }
